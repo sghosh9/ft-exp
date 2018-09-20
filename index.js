566dee8ce4d10c985346bf2d6eb11e00f9a6f12e
Sumo <somedutta@fusioncharts.com>
2018-09-20 17:38:10 +0530

    with sample

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-var dataset = [
-  [382, 'Washington Square E', 265, 1],
-  [372, 'Broadway & E 14 St', 439, 2],
-  [591, 'Perry St & Bleecker St', 251, 2],
-  [583, 'E 11 St & Broadway', 284, 1],
-  [223, 'Allen St & Rivington St', 439, 1],
-  [541, 'Warren St & Church St', 331, 1],
-  [354, 'E 19 St & 3 Ave', 439, 1],
-  [916, 'Emerson Pl & Myrtle Ave', 395, 1],
-  [277, 'Mercer St & Bleecker St', 369, 1],
-];
-
-var mainView = new A('mainView', undefined, dataset);
-
-var tripDurationView = Operations.createChild('Trip Duration', mainView, [['filter', row => parseInt(row[0]) > 500]]);
-var stationStartLetter = Operations.createChild('Station Start Letter', mainView, [['filter', row => row[1].toLowerCase().startsWith('w')]]);
-
-var gender2 = Operations.createChild('Gender is 2', tripDurationView, [['filter', row => parseInt(row[3]) === 2]]);
-
-// console.log(tripDurationView);
-// console.log(stationStartLetter);
-// console.log(gender2);
+var dataset = [];
+// dataset = [
+//   [382, 'Washington Square E', 265, 1],
+//   [372, 'Broadway & E 14 St', 439, 2],
+//   [591, 'Perry St & Bleecker St', 251, 2],
+//   [583, 'E 11 St & Broadway', 284, 1],
+//   [223, 'Allen St & Rivington St', 439, 1],
+//   [541, 'Warren St & Church St', 331, 1],
+//   [354, 'E 19 St & 3 Ave', 439, 1],
+//   [916, 'Emerson Pl & Myrtle Ave', 395, 1],
+//   [277, 'Mercer St & Bleecker St', 369, 1],
+// ];
 
 
-var interpreter = function(payload) {
-  Operations.query(this, [['filter', row => row[payload.data.index] > payload.data.value]], true);
-}
+fetch('sample1.csv')
+.then(response => response.text())
+.then(str => {
+  dataset = str.split('\n').map(row => {
+    return row.split(',');
+  });
+  dataset.pop();
 
-mainView.setInterpreter(interpreter);
+  // console.log(dataset);
 
-// var c = Operations.createChild('c', a, (obj, row) => obj.data[row][1] < 5000000);
+  var mainView = new A('mainView', undefined, dataset);
 
-// var d = Operations.createChild('d', c, (obj, row) => obj.data[row][1] > 10000);
-// var e = Operations.createChild('e', c, (obj, row) => obj.data[row][1] > 40000);
-// var f = Operations.createChild('f', c, (obj, row) => obj.data[row][1] > 80000);
+  var tripDurationView = Operations.createChild('Trip duration more than 500', mainView, [['filter', row => parseInt(row[0]) > 500]]);
+  var stationStartLetter = Operations.createChild('Station starts with W', mainView, [['filter', row => row[1].toLowerCase().startsWith('w')]]);
 
-// var g = Operations.createChild('g', b, (obj, row) => obj.data[row][1] > 80000);
-// var h = Operations.createChild('h', b, (obj, row) => obj.data[row][1] > 80000);
-// // var g = createChild('g', b);
-// // var h = createChild('h', b);
+  var gender2 = Operations.createChild('Gender is 2', tripDurationView, [['filter', row => parseInt(row[3]) === 2]]);
 
-// // var i = createChild('i', d);
-// // var j = createChild('j', d);
+  console.log(mainView);
+  console.log(tripDurationView);
+  console.log(stationStartLetter);
+  console.log(gender2);
 
-// // var k = createChild('k', f);
 
-// // var l = createChild('l', k);
+  var interpreter = function(payload) {
+    Operations.query(this, [['filter', row => row[payload.data.index] > payload.data.value]], true);
+  }
 
-gender2.propagate({index: 2, value: 300});
+  mainView.setInterpreter(interpreter);
+
+  // propagate a change/payload manually for demo
+  // end station id = 500
+  gender2.propagate({index: 2, value: 500});
+});
